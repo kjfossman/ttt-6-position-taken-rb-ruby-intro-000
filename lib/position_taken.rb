ae1796bd809dc419152f0e621d8048bd34f130d2
Kyle Fossman <kjfossman@gmail.com>
2020-12-15 17:42:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,9 +3,9 @@
 
 def position_taken?(board, index)
 
-if board[index] == " " || "" || nil
+if board[index] == " " || board[index] == "" || board[index] ==  nil
   false
-else
+else 
   true
 end
 end
